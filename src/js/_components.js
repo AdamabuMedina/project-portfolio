e9ae88e6fe9e4c492820eb44b456d80db3123c50
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2024-07-31 16:32:50 +0300

    мобильная адаптация

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,1 +1,19 @@
-// console.log('components');
+const headerList = document.querySelector("#header-list");
+const headerLink = document.querySelectorAll(".header-list__link");
+
+console.log(headerLink);
+
+for (let i = 0; i < headerLink.length; i++) {
+  headerLink[i].addEventListener("click", function () {
+    // Находим текущий активный элемент
+    let current = document.querySelector(".header-list__link--active");
+
+    // Если активный элемент найден, удаляем у него класс
+    if (current) {
+      current.classList.remove("header-list__link--active");
+    }
+
+    // Добавляем класс к текущему элементу
+    this.classList.add("header-list__link--active");
+  });
+}
